ea0dbb5826fa60482aecfa09964fd3d5375b85d8
Edgar <mmnledgar@gmail.com>
2023-02-20 10:03:37 +0000

    setting up the default page

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,39 @@
+@extends('Home')
+
+
+
+@section("main")
+
+
+<div class="starter-stats">
+    <div class="blok">
+        <i class="fa fa-money"></i>
+        <div class="no">
+            <p>Asset Types</p>
+            <p>3</p>
+        </div>
+    </div>
+
+    <div class="blok">
+        <i class="fa fa-money kl"></i>
+        <div class="no">
+            <p>Total Gains</p>
+            <p>-100.01 BLONION</p>
+        </div>
+    </div>
+
+    <div class="blok">
+        <i class="fa fa-money pl"></i>
+        <div class="no">
+            <p>Longest HODL</p>
+            <p>8 Months</p>
+        </div>
+    </div>
+    <div class="clear"></div>
+    <div class="gains">
+        <canvas id="myChart"></canvas>
+    </div>
+
+</div>
+
+@endsection
